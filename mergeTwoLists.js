167be150a260bf372702b0b8c860e28f37afab3c
ahrke <david.duy.minh@gmail.com>
2019-11-20 20:10:35 -0500

    add mergeTwoLists.js. Faster than 23%, less space than 79%

--- mergeTwoLists.js
+++ mergeTwoLists.js
@@ -4,36 +4,29 @@ function ListNode(val, next = null) {
 }
 
 const mergeTwoLists = function(l1, l2) {
+  if (!l1 || !l2) return []
   let temp1 = l1;
   let temp2 = l2;
   let resultList;
   let listRoot;
-  if (temp1.val <= temp2.val) {
-    listRoot = new ListNode(temp1.val);
-    temp1 = temp1.next
-  } else {
-    listRoot = new ListNode(temp2.val);
-    temp2 = temp2.next
-  }
 
-  resultList = listRoot;
-
-  while (temp1 !== null && temp2 !== null) {
+  while (temp1 || temp2) {
     let lowestVal;
-    if (temp1.val <= temp2.val || !temp2) {
+    if ((temp1 && temp1.val <= temp2.val) || !temp2) {
       lowestVal = temp1.val;
       temp1 = temp1.next
     } else {
       lowestVal = temp2.val;
       temp2 = temp2.next
     }
-    resultList.next = new ListNode(lowestVal);
-    resultList = resultList.next;
-  }
 
-  while (listRoot) {
-    console.log(listRoot)
-    listRoot = listRoot.next
+    if (!resultList) {
+      listRoot = new ListNode(lowestVal);
+      resultList = listRoot
+    } else {
+      resultList.next = new ListNode(lowestVal);
+      resultList = resultList.next;
+    }
   }
 
   return listRoot;
@@ -48,3 +41,4 @@ let l2second = new ListNode(3, l2third)
 let l2first = new ListNode(1, l2second)
 
 console.log(mergeTwoLists(l1first,l2first))
+console.log(mergeTwoLists())
